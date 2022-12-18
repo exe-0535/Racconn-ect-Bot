7ba9d5920aea48dbcf61e085914cf779c249ff7e
exe <igorwdowiczak@gmail.com>
2022-12-18 11:37:46 +0100

    Fixed player module err

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
-const { Player } = require('@discord-player');
+const { Player } = require('discord-player');
 
 dotenv.config();
 const TOKEN = process.env.TOKEN
