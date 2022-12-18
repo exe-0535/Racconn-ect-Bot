ceeb5eac411946a3389e3d23fb72549788dfc0fc
exe <igorwdowiczak@gmail.com>
2022-12-18 11:13:07 +0100

    Deploying slash commands added

--- index.js
+++ index.js
@@ -28,6 +28,29 @@ client.Player = new Player(client, {
         highWaterMark: 1 << 25
     }
 });
+
+let commands = []
+
+const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"));
+for (const file of slashFiles) {
+    const slashcmd = require('./slash/${file}');
+    client.slashcommands.set(slashcmd.data.name, slashcmd);
+    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON());
+}
+
+if (LOAD_SLASH) {
+    const rest = new REST({ version: "9" }).setToken(TOKEN);
+    console.log("Deploying slash commands");
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+        .then(() => {
+            console.log("Successfully loaded");
+            process.exit(0);
+        })
+        .catch((err) => {
+            if (err) console.log(err);
+            process.exit(1);
+        });
+}
 // client.once('ready', () => console.log('Bot is online!'));
 
 // client.on('error', console.error);
