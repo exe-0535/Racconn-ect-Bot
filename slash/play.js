e1aecd22b962dfa68e8ba6de23d82c15ccc49842
exe <igorwdowiczak@gmail.com>
2022-12-25 17:42:44 +0100

    Bot can now play YouTube songs from link or searchterms

--- slash/play.js
+++ slash/play.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
-const { MessageEmbed } = require("discord.js");
-const { QueryType } = require("discord-player");
+const { MessageEmbed, CommandInteraction } = require("discord.js");
+const { QueryType, Player } = require("discord-player");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,12 +10,6 @@ module.exports = {
             subcommand.setName("song")
                 .setDescription("Odtwarza utwór z podanego linku")
                 .addStringOption((option) => option.setName("url").setDescription("Link do utworu").setRequired(true))
-        )
-        .addSubcommand((subcommand) =>
-            subcommand
-                .setName("playlist")
-                .setDescription("Odtwarza playlistę z podanego linku")
-                .addStringOption((option) => option.setName("url").setDescription("Link do playlisty").setRequired(true))
         )
         .addSubcommand((subcommand) =>
             subcommand.setName("search").setDescription("Wyszukuje i odtwarza utwór na postawie podanych słów kluczowych")
@@ -48,10 +42,22 @@ module.exports = {
                 .setThumbnail(song.thumbnail)
                 .setFooter({ text: `Długość: ${song.duration}` })
 
-        } else if (interaction.options.getSubcommand() === "playlist") {
-
         } else if (interaction.options.getSubcommand() === "search") {
+            let url = interaction.options.getString("searchterms")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.AUTO
+            })
 
+            if (result.tracks.length === 0)
+                return interaction.editReply("Nie znaleziono podanego utworu");
+
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** dodano do kolejki`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Długość: ${song.duration}` })
         }
         if (!queue.playing) {
             await queue.play();
