905adc4f6e5608b30ec806b34080b7ab4c3e32b3
exe <igorwdowiczak@gmail.com>
2022-12-25 18:00:15 +0100

    queue.js is now ACTUALLY handling errors

--- slash/queue.js
+++ slash/queue.js
@@ -5,11 +5,19 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("queue")
         .setDescription("Wyświetla bieżącą kolejkę odtwarzania")
-        .addNumberOption((option) => option.setName("strona").setDescription("Numer strony kolejki").setMinValue(1)),
+        .addNumberOption((option) => option.setName("page").setDescription("Numer strony kolejki").setMinValue(1)),
 
     run: async ({ client, interaction }) => {
         const queue = client.getQueue(interaction.guildId);
 
+        if (!queue || !queue.playing) {
+            return await interaction.editReply("Brak utworów w kolejce")
+        }
 
+        const totalPages = Math.ceil(queue.tracks.length / 10) || 1
+        const page = (interaction.options.getNumber("page") || 1) - 1
+
+        if (page > totalPages)
+            return await interaction.editReply(`Nieprawidłowa strona. Aktualnie jest ${totalPages} stron kolejki`)
     }
 }
