d4e9385d6cbdeb089a6683c24ce8250cc3d6beb3
exe <igorwdowiczak@gmail.com>
2022-12-27 17:32:08 +0100

    ✨ Made cosmetic changes in /queue command

--- slash/queue.js
+++ slash/queue.js
@@ -4,21 +4,37 @@ const { MessageEmbed, CommandInteraction } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("queue")
-        .setDescription("Wyświetla bieżącą kolejkę odtwarzania")
+        .setDescription(":raccoon: Wyświetla bieżącą kolejkę odtwarzania")
         .addNumberOption((option) => option.setName("page").setDescription("Numer strony kolejki").setMinValue(1)),
 
     run: async ({ client, interaction }) => {
+
+        let embed = new EmbedBuilder();
+
         const queue = client.player.getQueue(interaction.guildId);
 
         if (!queue || !queue.playing) {
-            return await interaction.editReply("Brak utworów w kolejce")
+            return await interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle(":raccoon: Brak utworów w kolejce")
+                ]
+            })
         }
 
         const totalPages = Math.ceil(queue.tracks.length / 10) || 1
         const page = (interaction.options.getNumber("page") || 1) - 1
 
-        if (page > totalPages)
-            return await interaction.editReply(`Nieprawidłowa strona. Aktualnie jest ${totalPages} stron kolejki`);
+        if (page > totalPages) {
+            return await interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle(`:raccoon: Nieprawidłowa strona. Aktualnie jest ${totalPages} stron kolejki`)
+                ]
+            });
+        }
 
         const queueString = queue.tracks.slice(page * 10, page * 10 + 10).map((song, i) => {
             return `**${page * 10 + i + 1}.** \`[${song.duration}]\` ${song.title} -- <@${song.requestedBy.id}>`
@@ -26,14 +42,14 @@ module.exports = {
 
         const currentSong = queue.current
 
-        let embed = new EmbedBuilder();
 
         await interaction.editReply({
             embeds: [
                 embed
-                    .setDescription(`**Aktualnie odtwarzane:**\n` +
+                    .setColor(0xFFFFFF)
+                    .setDescription(`**Aktualnie odtwarzam:**\n` +
                         (currentSong ? `\`[${currentSong.duration}]\` ${currentSong.title} -- <@${currentSong.requestedBy.id}>` : "None") +
-                        `\n\n**Kolejka odtwarzania**\n${queueString}`
+                        `\n\n**Następnie:**\n${queueString}`
                     )
                     .setFooter({
                         text: `Strona ${page + 1} z ${totalPages}`
