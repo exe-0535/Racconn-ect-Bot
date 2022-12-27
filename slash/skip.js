a46fa3cadc03d65354c04f7e9e744448d50a0280
exe <igorwdowiczak@gmail.com>
2022-12-27 13:40:58 +0100

    ⚓ Added /skip command

---
+++ slash/skip.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
+const { MessageEmbed, CommandInteraction } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("skip")
+        .setDescription("Przesuwa kolejkę o jeden utwór w przód"),
+
+    run: async ({ client, interaction }) => {
+
+        const embed = new EmbedBuilder();
+
+        const queue = client.player.getQueue(interaction.guildId);
+
+        if (!queue || queue.tracks.length === 0) {
+            return await interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle("Brak utworów w kolejce")
+                ]
+            });
+        }
+
+        queue.skip();
+
+        const song = queue.current;
+        return await interaction.editReply({
+            embeds: [
+                embed
+                    .setColor(0xFFFFFF)
+                    .setTitle(`Skipped to: ${song.title}`)
+                    .setDescription(`By ${song.author}`)
+            ]
+        });
+    }
+}
