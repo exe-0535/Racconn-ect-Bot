1b8894a34d505ff807aeb9ec188e5dbad1c546c7
exe <igorwdowiczak@gmail.com>
2022-12-27 17:35:16 +0100

    ✨ Made cosmetic changes in /quit command

--- slash/quit.js
+++ slash/quit.js
@@ -4,14 +4,25 @@ const { CommandInteraction } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("quit")
-        .setDescription("Rozłącza bota z kanału głosowego"),
+        .setDescription(":raccoon: Rozłącza bota z kanału głosowego"),
     run: async ({ client, interaction }) => {
         const queue = client.player.getQueue(interaction.guild);
+        let embed = new EmbedBuilder();
         if (queue) {
             queue.stop();
-            interaction.editReply("Rozłączono z czatem głosowym");
+            interaction.editReply({
+                embeds:
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle("Rozłączono z czatem głosowym")
+            });
         } else {
-            interaction.editReply("Ten bot nie znajduje się na czacie głosowym");
+            interaction.editReply({
+                embeds:
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle("Ten bot nie znajduje się na czacie głosowym")
+            });
         }
     },
 };
