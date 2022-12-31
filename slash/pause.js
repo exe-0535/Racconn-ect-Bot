5628c0f1c375f497515e7e95ea35ffd1a20ba8f6
exe <igorwdowiczak@gmail.com>
2022-12-31 16:09:45 +0100

    ➕ Added pause.js, resume.js and skipto.js files. Changes yet to come.

---
+++ slash/pause.js
@@ -0,0 +1,8 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
+const { MessageEmbed, CommandInteraction } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("pause")
+        .setDescription("Wstrzymuje odtwarzany utwór")
+}
---
+++ slash/resume.js
@@ -0,0 +1,8 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
+const { MessageEmbed, CommandInteraction } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("resume")
+        .setDescription("Ponownie odtwarza zatrzymany utwór")
+}
