ea3487f37ae74d422357e101836624b179daa43d
exe <igorwdowiczak@gmail.com>
2023-01-01 17:15:50 +0100

    ⚓ /pause command should now be able to pause the queue

--- slash/pause.js
+++ slash/pause.js
@@ -4,5 +4,41 @@ const { MessageEmbed, CommandInteraction } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("pause")
-        .setDescription("Wstrzymuje odtwarzany utwór")
+        .setDescription("Wstrzymuje odtwarzany utwór"),
+    run: async ({ client, interaction }) => {
+
+        let embed = new EmbedBuilder();
+
+        const queue = client.player.getQueue(interaction.guild);
+
+        if (!queue || queue.tracks.length === 0) {
+            return await interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle("Brak utworów w kolejce")
+                ]
+            });
+        }
+
+        if (!interaction.member.voice.channel) {
+            return interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle(":raccoon: Musisz być połączony z czatem głosowym, aby użyć tej komendy")
+                ]
+            });
+        }
+
+        queue.setPaused(true);
+
+        return await interaction.editReply({
+            embeds: [
+                embed
+                    .setColor(0xFFFFFF)
+                    .setTitle("Zatrzymano odtwarzanie kolejki")
+            ]
+        });
+    }
 }
