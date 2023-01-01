3f6a49983f0503f2c93dfb61321068ded13e59c9
exe <igorwdowiczak@gmail.com>
2023-01-01 17:52:07 +0100

    ⚓ Bot will now resume playing the queue on /resume command

--- slash/resume.js
+++ slash/resume.js
@@ -4,5 +4,41 @@ const { MessageEmbed, CommandInteraction } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("resume")
-        .setDescription("Ponownie odtwarza zatrzymany utwór")
-}
+        .setDescription("Ponownie odtwarza zatrzymany utwór"),
+    run: async ({ client, interaction }) => {
+
+        let embed = new EmbedBuilder();
+
+        const queue = client.player.getQueue(interaction.guild);
+
+        if (!queue) {
+            return await interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle("Brak utworów w kolejce")
+                ]
+            });
+        }
+
+        if (!interaction.member.voice.channel) {
+            return interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle(":raccoon: Musisz być połączony z czatem głosowym, aby użyć tej komendy")
+                ]
+            });
+        }
+
+        queue.setPaused(false);
+
+        return await interaction.editReply({
+            embeds: [
+                embed
+                    .setColor(0xFFFFFF)
+                    .setTitle("Ponownie odtwarzam kolejkę")
+            ]
+        });
+    }
+} 
