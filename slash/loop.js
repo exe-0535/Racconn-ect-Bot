2e61ab456e21a66969391cd5491db01e152cc6c4
exe <igorwdowiczak@gmail.com>
2023-01-02 23:16:06 +0100

    /loop can now be run yet it's functionality is uncertain (pre-alpha build)

--- slash/loop.js
+++ slash/loop.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
+const { QueueRepeatMode } = require("discord-player");
 const { MessageEmbed, CommandInteraction } = require("discord.js");
 
 module.exports = {
@@ -7,5 +8,55 @@ module.exports = {
         .setDescription("Zapętluje obecnie grający utwór"),
     run: async ({ interaction, client }) => {
 
+        let embed = new EmbedBuilder();
+
+        const queue = client.player.getQueue(interaction.guild);
+
+        if (!interaction.member.voice.channel) {
+            return interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle(":raccoon: Musisz być połączony z czatem głosowym, aby użyć tej komendy")
+                ]
+            });
+        }
+
+        if (!queue) {
+            return await interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle("Brak utworów w kolejce")
+                ]
+            });
+        }
+
+        try {
+
+            if (queue.setRepeatMode(QueueRepeatMode.OFF) === true) {
+                queue.setRepeatMode(QueueRepeatMode.TRACK);
+                return interaction.editReply({
+                    embeds: [
+                        embed
+                            .setColor(0xFFFFFF)
+                            .setTitle("Zapętlam obecnie grający utwór. By wyłączyć zapętlenie użyj /loop")
+                    ]
+                });
+            }
+
+            if (queue.setRepeatMode(QueueRepeatMode.TRACK) === true) {
+                queue.setRepeatMode(QueueRepeatMode.OFF);
+                return interaction.editReply({
+                    embeds: [
+                        embed
+                            .setColor(0xFFFFFF)
+                            .setTitle("Ponownie odtwarzam kolejkę")
+                    ]
+                });
+            }
+        } catch (e) {
+            console.log(e);
+        }
     }
 }
