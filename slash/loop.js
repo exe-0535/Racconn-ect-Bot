509bb65c6609eaf37e1f7001f7a9eb201962a747
exe <igorwdowiczak@gmail.com>
2023-01-04 20:40:58 +0100

    ⚓ Optimized loop.js code

--- slash/loop.js
+++ slash/loop.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
 const { QueueRepeatMode } = require("discord-player");
-const { MessageEmbed, CommandInteraction } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -32,33 +31,28 @@ module.exports = {
             });
         }
 
-        try {
-
-            // Check if the repeat mode is currently set to OFF
-            if (queue.repeatMode === QueueRepeatMode.OFF) {
-                // If it is, set it to TRACK and send a message
-                queue.setRepeatMode(QueueRepeatMode.TRACK);
-                return interaction.editReply({
-                    embeds: [
-                        embed
-                            .setColor(0xFFFFFF)
-                            .setTitle("Zapętlam obecnie grający utwór. By wyłączyć zapętlenie użyj /loop")
-                    ]
-                });
-            }
-            // If repeat mode is already set to TRACK, set it to OFF and send a message
-            else if (queue.repeatMode === QueueRepeatMode.TRACK) {
-                queue.setRepeatMode(QueueRepeatMode.OFF);
-                return interaction.editReply({
-                    embeds: [
-                        embed
-                            .setColor(0xFFFFFF)
-                            .setTitle("Resuming normal queue playback")
-                    ]
-                });
-            }
-        } catch (e) {
-            console.log(e);
+        // Check if the repeat mode is currently set to OFF
+        if (queue.repeatMode === QueueRepeatMode.OFF) {
+            // If it is, set it to TRACK and send a message
+            queue.setRepeatMode(QueueRepeatMode.TRACK);
+            return interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle("Zapętlam obecnie grający utwór. By wyłączyć zapętlenie użyj /loop")
+                ]
+            });
+        }
+        // If repeat mode is already set to TRACK, set it to OFF and send a message
+        else if (queue.repeatMode === QueueRepeatMode.TRACK) {
+            queue.setRepeatMode(QueueRepeatMode.OFF);
+            return interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle("Ponownie odtwarzam kolejkę")
+                ]
+            });
         }
     }
 }
