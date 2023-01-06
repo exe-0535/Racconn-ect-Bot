0640dd306cb570b9083aaf781e51249ad6991d83
exe <igorwdowiczak@gmail.com>
2023-01-06 12:38:28 +0100

    :anchor: Made significant changes to the /play command. It is now only able to play individual spotify tracks but with only one command

--- slash/play.js
+++ slash/play.js
@@ -1,19 +1,11 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
-const { QueryType } = require("discord-player");
+const { QueryType, Playlist, Track } = require("discord-player");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("play")
         .setDescription(":raccoon: Odtwarza utwór z YouTube bądź Spotify")
-        .addSubcommand((subcommand) =>
-            subcommand.setName("song")
-                .setDescription(":raccoon: Odtwarza utwór z podanego linku")
-                .addStringOption((option) => option.setName("url").setDescription("Link do utworu").setRequired(true))
-        )
-        .addSubcommand((subcommand) =>
-            subcommand.setName("search").setDescription(":raccoon: Wyszukuje i odtwarza utwór na postawie podanych słów kluczowych")
-                .addStringOption((option) => option.setName("searchterms").setDescription("Słowa kluczowe do wyszukiwania").setRequired(true))
-        ),
+        .addStringOption((option) => option.setName("insert").setDescription("Link bądź słowa kluczowe").setRequired(true)),
 
     run: async ({ client, interaction }) => {
         // EmbedBuilder
@@ -44,17 +36,20 @@ module.exports = {
         // Bot connection to the voice channel
         if (!QUEUE.connection) await QUEUE.connect(interaction.member.voice.channel)
 
+        // Getting the users input
+        let insert = interaction.options.getString("insert");
 
-        if (interaction.options.getSubcommand() === "song") {
-            let url = interaction.options.getString("url");
-            if (url.startsWith("https://open.spotify.com/track/")) {
-                // code to handle Spotify link
-                const result = await client.player.search(url, {
+        try {
+            // Handling SPOTIFY SONG
+            if (insert.startsWith("https://open.spotify.com/track/")) {
+
+                const SEARCH_RESULT = await client.player.search(insert, {
                     requestedBy: interaction.user,
                     searchEngine: QueryType.SPOTIFY_SONG
                 })
 
-                if (result.tracks.length === 0)
+                // If no results were found
+                if (SEARCH_RESULT.tracks.length === 0) {
                     return interaction.editReply({
                         embeds: [
                             embed
@@ -62,61 +57,118 @@ module.exports = {
                                 .setTitle(":raccoon: Nie znaleziono podanego utworu")
                         ]
                     });
+                }
 
-                const song = result.tracks[0];
+                // Declaring song searched for
+                const SONG = SEARCH_RESULT.tracks[0];
 
-                await QUEUE.addTrack(song);
+                // Adding the song to the queue
+                await QUEUE.addTrack(SONG);
                 embed
                     .setColor(0xFFFFFF)
-                    .setTitle(`${song.title}`)
+                    .setTitle(`${SONG.title}`)
                     .setDescription(`dodano do kolejki`)
-                    .setThumbnail(song.thumbnail)
-                    .setFooter({ text: `Długość: ${song.duration} ` });
-            } else {
-                // code to handle YouTube link
-                const result = await client.player.search(url, {
-                    requestedBy: interaction.user,
-                    searchEngine: QueryType.YOUTUBE_VIDEO
-                })
-                if (result.tracks.length === 0)
-                    return interaction.editReply({
-                        embeds: [
-                            embed
-                                .setColor(0xFFFFFF)
-                                .setTitle(":raccoon: Nie znaleziono podanego utworu")
-                        ]
-                    });
-
-                const song = result.tracks[0];
-                await QUEUE.addTrack(song);
-
-                embed
-                    .setColor(0xFFFFFF)
-                    .setTitle(`${song.title}`)
-                    .setDescription(`dodano do kolejki`)
-                    .setThumbnail(song.thumbnail)
-                    .setFooter({ text: `Długość: ${song.duration} ` });
+                    .setThumbnail(SONG.thumbnail)
+                    .setFooter({ text: `Długość: ${SONG.duration} ` });
             }
 
-        } else if (interaction.options.getSubcommand() === "search") {
-            let url = interaction.options.getString("searchterms")
-            const result = await client.player.search(url, {
-                requestedBy: interaction.user,
-                searchEngine: QueryType.AUTO
-            })
+            // Handling SPOTIFY ALBUM AND PLAYLIST
+            // Since discord-player handles it like the same case in switch:
+            // case QueryType.SPOTIFY_PLAYLIST:
+            // case QueryType.SPOTIFY_ALBUM:
+            if (insert.startsWith("https://open.spotify.com/playlist/") || insert.startsWith("https://open.spotify.com/album/")) {
+                const SEARCH_RESULT = await client.player.search(insert, {
+                    requestedBy: interaction.user,
+                    searchEngine: QueryType.SPOTIFY_PLAYLIST
+                })
+            }
 
-            if (result.tracks.length === 0)
-                return interaction.editReply(":raccoon: Nie znaleziono podanego utworu");
+            // Handling YOUTUBE PLAYLIST
+            if (insert.startsWith("https://youtube.com/playlist?")) {
+                const SEARCH_RESULT = await client.player.search(insert, {
+                    requestedBy: interaction.user,
+                    searchEngine: QueryType.YOUTUBE_PLAYLIST
+                })
+            }
 
-            const song = result.tracks[0]
-            await queue.addTrack(song)
-            embed
-                .setColor(0xFFFFFF)
-                .setTitle(`${song.title}`)
-                .setDescription(`dodano do kolejki`)
-                .setThumbnail(song.thumbnail)
-                .setFooter({ text: `Długość: ${song.duration} ` });
+            // Handling TYPOS, OTHER SEARCHES FOR INDIVIDUAL TRACKS
+
+        } catch (e) {
+            console.log(e);
         }
+
+        // if (interaction.options.getSubcommand() === "song") {
+        //     let url = interaction.options.getString("url");
+        //     if (url.startsWith("https://open.spotify.com/track/")) {
+        //         // code to handle Spotify link
+        //         const result = await client.player.search(url, {
+        //             requestedBy: interaction.user,
+        //             searchEngine: QueryType.SPOTIFY_SONG
+        //         })
+
+        //         if (result.tracks.length === 0)
+        //             return interaction.editReply({
+        //                 embeds: [
+        //                     embed
+        //                         .setColor(0xFFFFFF)
+        //                         .setTitle(":raccoon: Nie znaleziono podanego utworu")
+        //                 ]
+        //             });
+
+        //         const song = result.tracks[0];
+
+        //         await QUEUE.addTrack(song);
+        //         embed
+        //             .setColor(0xFFFFFF)
+        //             .setTitle(`${song.title}`)
+        //             .setDescription(`dodano do kolejki`)
+        //             .setThumbnail(song.thumbnail)
+        //             .setFooter({ text: `Długość: ${song.duration} ` });
+        //     } else {
+        //         // code to handle YouTube link
+        //         const result = await client.player.search(url, {
+        //             requestedBy: interaction.user,
+        //             searchEngine: QueryType.YOUTUBE_VIDEO
+        //         })
+        //         if (result.tracks.length === 0)
+        //             return interaction.editReply({
+        //                 embeds: [
+        //                     embed
+        //                         .setColor(0xFFFFFF)
+        //                         .setTitle(":raccoon: Nie znaleziono podanego utworu")
+        //                 ]
+        //             });
+
+        //         const song = result.tracks[0];
+        //         await QUEUE.addTrack(song);
+
+        //         embed
+        //             .setColor(0xFFFFFF)
+        //             .setTitle(`${song.title}`)
+        //             .setDescription(`dodano do kolejki`)
+        //             .setThumbnail(song.thumbnail)
+        //             .setFooter({ text: `Długość: ${song.duration} ` });
+        //     }
+
+        //     } else if(interaction.options.getSubcommand() === "search") {
+        //     let url = interaction.options.getString("searchterms")
+        //     const result = await client.player.search(url, {
+        //         requestedBy: interaction.user,
+        //         searchEngine: QueryType.AUTO
+        //     })
+
+        //     if (result.tracks.length === 0)
+        //         return interaction.editReply(":raccoon: Nie znaleziono podanego utworu");
+
+        //     const song = result.tracks[0]
+        //     await queue.addTrack(song)
+        //     embed
+        //         .setColor(0xFFFFFF)
+        //         .setTitle(`${song.title}`)
+        //         .setDescription(`dodano do kolejki`)
+        //         .setThumbnail(song.thumbnail)
+        //         .setFooter({ text: `Długość: ${song.duration} ` });
+        // }
 
         // Starts playing the queue
         if (!QUEUE.playing) {
