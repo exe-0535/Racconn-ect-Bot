d0d9290684333dcd9cf45d3ea58b5892e19a575d
exe <igorwdowiczak@gmail.com>
2023-01-08 11:38:08 +0100

    :anchor: Added first choices of sound effect to the /sfx_on command

--- slash/effect.js
+++ slash/effect.js
@@ -3,11 +3,51 @@ const { AudioFilters } = require("discord-player");
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("effect")
-        .setDescription("Aktywuje wprowadzony efekt dźwiękowy na odtwarzanym pliku")
-        .addStringOption((option) => option.setName("sfx").setDescription("Nazwa efektu").setRequired(true)),
+        .setName("sfx_on")
+        .setDescription("Aktywuje wprowadzony efekt dźwiękowy na odtwarzanym utworze")
+        .addStringOption((option) => option.setName("sfx").setDescription("Nazwa efektu").setRequired(true)
+            .addChoices(
+                { name: "8D", value: "8D" },
+                { name: "BASS BOOST", value: "bassboost" },
+                { name: "VAPORWAVE", value: "vaporwave" },
+                { name: "PHASER", value: "phaser" },
+                { name: "NIGHTCORE", value: "nightcore" },
+                { name: "CHORUS", value: "chorus" }
+            )
+        ),
 
     run: async ({ client, interaction }) => {
+        // EmbedBuilder
+        let embed = new EmbedBuilder();
+
+        // Check if user is on voice
+        if (!interaction.member.voice.channel) {
+            return interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle(":raccoon: Musisz być połączony z czatem głosowym, aby użyć tej komendy")
+                ]
+            });
+        }
+
+        // Getting queue
+        const queue = client.player.getQueue(interaction.guild);
+
+        // Check if queue is active
+        if (!queue) {
+            return await interaction.editReply({
+                embeds: [
+                    embed
+                        .setColor(0xFFFFFF)
+                        .setTitle("Brak utworów w kolejce")
+                ]
+            });
+        }
+
+        // Getting the users input
+        let sfx = interaction.options.getString("sfx").toLowerCase();
+
 
     }
 }
