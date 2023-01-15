8e64dcad06b72bf04c761abaa8bca6a5f884f5bf
exe <igorwdowiczak@gmail.com>
2023-01-15 13:04:07 +0100

    :anchor: Moved CLIENT_ID and GUILD_ID fields values to .env

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const TOKEN = process.env.TOKEN;
 
 const LOAD_SLASH = process.argv[2] == "load";
 
-const CLIENT_ID = "1053721800028196905";
-const GUILD_ID = "906281600923340851";
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
 
 const client = new Discord.Client({
     intents: [
