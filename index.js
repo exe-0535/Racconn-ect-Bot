58c72447019a7abc78b34c760f7edcd89fe75076
exe <igorwdowiczak@gmail.com>
2023-01-19 18:56:17 +0100

    :anchor: Bot now runs 100% correctly with only 1 command

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const { GatewayIntentBits } = require('discord.js');
 dotenv.config();
 const TOKEN = process.env.TOKEN;
 
-const LOAD_SLASH = process.argv[2] == "load";
-
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
@@ -38,43 +36,40 @@ const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js")
 for (const file of slashFiles) {
     const slashcmd = require(`./slash/${file}`);
     client.slashcommands.set(slashcmd.data.name, slashcmd);
-    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON());
+    commands.push(slashcmd.data.toJSON());
 }
 
-if (LOAD_SLASH) {
-    const rest = new REST({ version: "9" }).setToken(TOKEN);
-    console.log("Deploying slash commands");
-    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
-        .then(() => {
-            console.log("Successfully loaded");
-            process.exit(0);
-        })
-        .catch((err) => {
-            if (err) console.log(err);
-            process.exit(1);
-        });
-}
-else {
-    client.on("ready", () => {
-        console.log(`Logged in as ${client.user.tag}`);
+const rest = new REST({ version: "9" }).setToken(TOKEN);
+console.log("Deploying slash commands");
+rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+    .then(() => {
+        console.log("Successfully loaded");
     })
-    client.on("interactionCreate", (interaction) => {
-        async function handleCommand() {
-            if (!interaction.isCommand()) return;
+    .catch((err) => {
+        if (err) console.log(err);
+        process.exit(1);
+    });
 
-            const slashcmd = client.slashcommands.get(interaction.commandName);
-            if (!slashcmd) interaction.reply("Not a valid slash command");
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+})
+client.on("interactionCreate", (interaction) => {
+    async function handleCommand() {
+        if (!interaction.isCommand()) return;
 
-            await interaction.deferReply();
-            await slashcmd.run({ client, interaction });
-        }
-        handleCommand();
-    })
-    // If you want to test out your bot
-    client.on("messageCreate", (message) => {
-        if (message.content == "Is my raccoon listening?") {
-            message.reply("Listening.");
-        }
-    })
-    client.login(TOKEN);
-}
+        const slashcmd = client.slashcommands.get(interaction.commandName);
+        if (!slashcmd) interaction.reply("Not a valid slash command");
+
+        await interaction.deferReply();
+        await slashcmd.run({ client, interaction });
+    }
+    handleCommand();
+})
+
+// If you want to test out your bot
+client.on("messageCreate", (message) => {
+    if (message.content == "Is my raccoon listening?") {
+        message.reply("Listening.");
+    }
+})
+client.login(TOKEN);
