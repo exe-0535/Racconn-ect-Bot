c07f5a5b241c1f8474db7c4ca4285d07f6d08187
exe <igorwdowiczak@gmail.com>
2023-01-21 17:22:13 +0100

    :anchor: Deleted whitespaces

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const { GatewayIntentBits } = require('discord.js');
 
 dotenv.config();
 const TOKEN = process.env.TOKEN;
-
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
